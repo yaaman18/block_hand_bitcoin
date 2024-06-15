93f848653fecd64f8c176262e14b243e5dfc8004
yaaman18 <gucchiyama8551@hotmail.co.jp>
2024-06-15 12:20:34 +0900

    master

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,11 @@
 	import bs58 from 'bs58';
 	import Header from './Header.svelte';
 	import { onMount } from 'svelte';
+	import MnemonicCard from './MnemonicCard.svelte';
 
 	let providedCode = '';
 	let passwordString = '';
-	let bitcoinPrivateKey = '';
+
 	let mnemonicPhrase = '';
 	let error = '';
 	let warning = '';
@@ -15,6 +16,21 @@
     let loader = document.getElementById('loader');
 	let mnemonicDisplay = document.getElementById('mnemonicDisplay');
 	let isCopied = false;
+	let isDialogOpen = false;
+	let dialog: { showModal: () => void; addEventListener: (arg0: string, arg1: (event: any) => void) => void; getBoundingClientRect: () => any; close: () => void; }
+
+
+	function openDialog() {
+    dialog.showModal()
+
+  }
+
+  function closeDialog() {
+	isCopied = false;
+    dialog.close()
+  }
+
+
 
 
 	function providedCodeInput() {
@@ -31,7 +47,7 @@
 
 	async function copyToClipboard(text: string) {
 		try {
-      await navigator.clipboard.writeText(text);
+        await navigator.clipboard.writeText(text);
 
       isCopied = true; // コピー完了メッセージを表示
     } catch (err) {
@@ -40,37 +56,38 @@
 	}
 
 	async function handleFormSubmit() {
-		validateInput(); // Base58形式の検証
-		providedCodeInput(); // Provided codeの長さ検証
-		passwordStringInput(); // Passwordの長さ検証
+    validateInput();
+    providedCodeInput();
+    passwordStringInput();
 
-		// 他の条件が満たされている場合のみ、バックエンドの関数を呼び出す
-		if (warning === '') {
-			try {
-				if (loader !== null) {
-                   loader.style.display = 'block'; // ローダーを表示
+    if (warning === '') {
+        try {
+            if (loader !== null) {
+                loader.style.display = 'block';
+            }
+            const result = await invoke<string>('generate_mnemonic_phrase', { providedCode, passwordString });
+            if (loader !== null) {
+                loader.style.display = 'none';
+            }
+            if (result) {
+
+                mnemonicPhrase = result[1];
+				  console.log("Generated Mnemonic Phrase:", mnemonicPhrase);
+                if (mnemonicDisplay !== null) {
+                    mnemonicDisplay.innerText = mnemonicPhrase;
                 }
-				const result = await invoke<[string, string]>('generate_hd_wallet_xprv', { providedCode, passwordString });
-				if (loader !== null) {
-                    loader.style.display = 'none'; // ローダーを非表示
-                }
-				if (Array.isArray(result) && result.length === 2) {
-					bitcoinPrivateKey = result[0]; // コンポーネントのステートを更新
-					mnemonicPhrase = result[1];
-					if (mnemonicDisplay !== null) {
-                        mnemonicDisplay.innerText = mnemonicPhrase;
-                    }
-				} else {
-					console.error('Result is not the expected format');
-				}
-			} catch (err) {
-				if(loader){
-					loader.style.display = 'none';
-				}
-				error = (err as Error).message;
-			}
-		}
-	}
+                isDialogOpen = true; // ダイアログを表示
+            } else {
+                console.error('Result is not the expected format');
+            }
+        } catch (err) {
+            if(loader){
+                loader.style.display = 'none';
+            }
+            error = (err as Error).message;
+        }
+    }
+}
 
 	// 入力検証を行う関数
 	function validateInput() {
@@ -99,7 +116,11 @@
 
 	function generateKeys() {
 		handleFormSubmit();
+		openDialog();
 	}
+
+
+
 
 	  onMount(() => {
     loader = document.getElementById('loader');
@@ -109,16 +130,18 @@
 </script>
 
 
+<div class="container">
+
 <Header title="Block Hand Bitcoin" />
 
-<main class="background">
 
-	<div class="container">
 		<div class="moveUp2"></div>
 
 		<h2>Let's start using the Bitcoin mnemonic phrase generator</h2>
-		<h4>Block Hand Bitcoin is an open-source application that generates mnemonic phrases.<br>
-			By combining the string engraved on your accessory with your own unique password, you can manage the mnemonics of your Bitcoin wallet.</h4>
+		<h4>
+			Block Hand Bitcoin is an open-source application that generates mnemonic phrases.<br>
+			By combining the string engraved on your accessory with your own unique password, you can manage the mnemonics of your Bitcoin wallet.
+		</h4>
 		<h3>
 			Please enter a password using characters other than
 			+, /, 0 (zero), O (uppercase 'o'), I (uppercase 'i'),
@@ -166,28 +189,25 @@
 			{/if}
 		</div>
 
-		<button on:click={generateKeys} disabled={isButtonDisabled}>Generate Keys</button>
+		<button class="generateKeys-button" on:click={generateKeys} disabled={isButtonDisabled}>Generate Keys</button>
 
 		{#if warning}
 			<p class="warning">{warning}</p>
 		{/if}
-
-
-		{#if mnemonicPhrase}
-			<p class="key">Mnemonic Phrase:</p>
-			<p class="mnemonic">{mnemonicPhrase}</p>
-			<button on:click={() => copyToClipboard(mnemonicPhrase)}>Copy to Clipboard</button>
-			 {#if isCopied}
-        <p class="copied-message">Copied!</p>
-      {/if}
-		{/if}
+<MnemonicCard
+	bind:dialog
+	bind:mnemonicPhrase={mnemonicPhrase}
+	bind:isCopied={isCopied}
+	copyToClipboard={copyToClipboard}
+	on:closeDialog={closeDialog}
+/>
 
 		{#if error}
 			<p class="error">{error}</p>
 		{/if}
-	</div>
-	<h4>©︎Vaultwear</h4>
-</main>
+
+	<h4 id="copywright">©︎Vaultwear</h4>
+</div>
 
 <style>
 	:root {
@@ -199,31 +219,7 @@
 	}
 
 
-	.background {
 
-		background-image: linear-gradient(
-			to right,
-		); /* グラデーション適用 */
-		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-		border-radius: 10px;
-		display: flex; /* Flexboxを有効にします */
-		flex-direction: column; /* 子要素を縦方向に配置します */
-		justify-content: center; /* 子要素を水平方向で中央に配置します */
-		align-items: center; /* 子要素を垂直方向で中央に配置します */
-		 position: absolute;
-	}
-
-	.container {
-		display: flex; /* Flexboxを有効にします */
-		flex-direction: column; /* 子要素を縦方向に配置します */
-		justify-content: center; /* 子要素を水平方向で中央に配置します */
-		align-items: center; /* 子要素を垂直方向で中央に配置します */
-		margin: 0 auto; /* ウィンドウ全体を中央に配置します */
-		font-family: 'Courier New', monospace;
-		width: 100%;
-        margin-bottom: 10vh;
-
-	}
 
 
   .moveUp2 {
@@ -267,10 +263,13 @@
 
 
      h2 {
+		margin-left: 10rem;
+		margin-right: 10rem;
 		margin-top: 6rem;
 		margin-bottom: 3rem;
 		font: 2rem "Fira Sans", sans-serif, bold;
 		color: #51b24d;
+		text-align: left;
 	 }
 	 h3{
 		font-size: 1rem;
@@ -281,10 +280,13 @@
 	   text-align: left;
 	 }
 	 h4{
-		width: 60%;
+
 color: var(--accent-color);
 margin-bottom: 4rem;
-   text-align: left;
+margin-left: 10rem;
+		margin-right: 10rem;
+
+  text-align: left;
 	 }
 
 
@@ -308,6 +310,12 @@ margin-bottom: 4rem;
     box-shadow: 0 0 3px #aca99b; /* フォーカス時のシャドウを追加 */
 }
 
+.generateKeys-button{
+	width: 140px;
+	display: block;
+  margin: auto;
+
+}
 
 	.warning,
 	.error {
@@ -337,34 +345,17 @@ margin-bottom: 4rem;
 }
 
    .password-input{
-   width: 90%;
    display: flex;
    justify-content: center;
    align-items: center;
    margin-bottom: 1.5rem;
-
    }
    .input-field{
-	background-color:#e0ddd8}
+	width: 40%;
+	background-color:#e0ddd8
+}
 
-	.key{
-		color: var(--accent-color);
-		width: 80%;
-		text-align: left;
 
-	}
-	.mnemonic{
-		color: var(--accent-color);
-		width: 80%;
-		text-align: left;
-		margin-bottom: 2vh;
-	}
-
-	.copied-message {
-    color: green;
-    font-size: 1.2em;
-    margin-top: 10px;
-  }
 
 	/* HTML: <div class="loader"></div> */
 .loader {
@@ -386,6 +377,16 @@ margin-bottom: 4rem;
   animation:
     l7-1 2s infinite linear,
     l7-2 2s infinite linear;
+}
+
+
+
+#copywright{
+margin-top: 4rem;
+ display: flex;
+   justify-content: center;
+   align-items: center;
+   margin-bottom: 1.5rem;
 }
 @keyframes l7-1 {
   0%,
